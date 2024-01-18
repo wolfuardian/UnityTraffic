afdc3641a7547e67158c6abbd9226028e346110f
wolfuardian <wolfuardian@gmail.com>
2024-01-18 23:24:40 +0800

    移除 AgentData 不必要的 Tooltip

--- Traffic/Assets/CrowdSample/Scripts/Runtime/Data/AgentData.cs
+++ Traffic/Assets/CrowdSample/Scripts/Runtime/Data/AgentData.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace CrowdSample.Scripts.Runtime.Data
@@ -6,23 +5,21 @@ namespace CrowdSample.Scripts.Runtime.Data
     [CreateAssetMenu(fileName = "AgentData", menuName = "CrowdWizard/Agent Data")]
     public class AgentData : ScriptableObject
     {
-        [Tooltip("代理預制件列表。")] public List<GameObject> agentPrefabs = new List<GameObject>();
+        public GameObject[] agentPrefabs;
 
-        [Header("代理許可權")] [Tooltip("代理的許可權ID。")]
-        public PermissionID permissionID = PermissionID.None;
+        [Header("代理許可權")] public PermissionID permissionID = PermissionID.None;
 
-        [Header("代理行為設置")] [Tooltip("代理的最小速度。")]
-        public float minSpeed = 4f;
+        [Header("代理行為設置")] public float minSpeed = 4f;
 
-        [Tooltip("代理的最大速度。")] public float maxSpeed = 6f;
+        public float maxSpeed = 6f;
 
-        [Tooltip("代理的角速度。")] public float angularSpeed = 100f;
+        public float angularSpeed = 100f;
 
-        [Tooltip("代理的加速度。")] public float acceleration = 5f;
+        public float acceleration = 5f;
 
-        [Tooltip("代理的轉彎半徑。")] public float turningRadius = 2f;
+        public float turningRadius = 2f;
 
-        [Tooltip("代理的停止距離。")] public float stoppingDistance = 1f;
+        public float stoppingDistance = 1f;
 
         public enum PermissionID
         {
