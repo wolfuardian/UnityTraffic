3225c7dfd4b82857b58ae6942bdcde22cb116f92
eos.chen <eos.chen@nadisystem.com>
2024-01-18 16:51:34 +0800

    修正 拖曳 Waypoint 沒有即時刷新所有 Gizmos 的錯誤

---
+++ Traffic/Assets/CrowdSample/Scripts/Editor/Crowd/WaypointGizmosEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using CrowdSample.Scripts.Utils;
+using CrowdSample.Scripts.Runtime.Agent;
+
+namespace CrowdSample.Scripts.Editor.Crowd
+{
+    [CustomEditor(typeof(WaypointGizmos))]
+    public class WaypointGizmosEditor : UnityEditor.Editor
+    {
+        private Vector3 previousPosition;
+
+        private void OnSceneGUI()
+        {
+            var waypointGizmos = (WaypointGizmos)target;
+            if (waypointGizmos.transform.position == previousPosition) return;
+            previousPosition = waypointGizmos.transform.position;
+
+            UnityEditorUtils.UpdateAllGizmos();
+        }
+    }
+}
