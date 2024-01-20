7de38e8d76f2c13f0eaed471190f9c9b3431646e
wolfuardian <wolfuardian@gmail.com>
2024-01-21 07:06:48 +0800

    更新 Unity 與 UnityEditor 通用方法

--- Traffic/Assets/CrowdSample/Scripts/Utils/UnityEditorUtils.cs
+++ Traffic/Assets/CrowdSample/Scripts/Utils/UnityEditorUtils.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
+using UnityEditor;
 
 namespace CrowdSample.Scripts.Utils
 {
-    public class UnityEditorUtils : UnityEditor.Editor
+    public class UnityEditorUtils : Editor
     {
         public static void UpdateAllGizmos()
         {
@@ -11,6 +14,51 @@ namespace CrowdSample.Scripts.Utils
             {
                 updatable.UpdateGizmo();
             }
+        }
+
+        public static void SetInspectorLock(bool lockState)
+        {
+            try
+            {
+                // Get the Inspector window type
+                var inspectorWindowType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+                if (inspectorWindowType == null)
+                {
+                    throw new InvalidOperationException("Could not find InspectorWindow type.");
+                }
+
+                // Get the current Inspector window instance
+                var inspectorWindow = EditorWindow.GetWindow(inspectorWindowType);
+                if (inspectorWindow == null)
+                {
+                    throw new InvalidOperationException("Could not find an open Inspector window.");
+                }
+
+                // Use reflection to set the lock state
+                var isLockedProperty =
+                    inspectorWindowType.GetProperty("isLocked", BindingFlags.Public | BindingFlags.Instance);
+                if (isLockedProperty == null)
+                {
+                    throw new InvalidOperationException("Could not find 'isLocked' property in InspectorWindow type.");
+                }
+
+                isLockedProperty.SetValue(inspectorWindow, lockState, null);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error setting Inspector lock state: " + ex.Message);
+            }
+        }
+
+        public static GUIStyle CreateHeaderStyle(FontStyle fontStyle, int fontSize)
+        {
+            var headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontStyle = fontStyle,
+                fontSize  = fontSize,
+                normal    = { textColor = EditorStyles.boldLabel.normal.textColor }
+            };
+            return headerStyle;
         }
     }
 }
--- Traffic/Assets/CrowdSample/Scripts/Utils/UnityUtils.cs
+++ Traffic/Assets/CrowdSample/Scripts/Utils/UnityUtils.cs
@@ -5,33 +5,6 @@ namespace CrowdSample.Scripts.Utils
 {
     public class UnityUtils
     {
-        public static bool LockInspector(bool isLocked)
-        {
-            if (isLocked) return true;
-            ActiveEditorTracker.sharedTracker.isLocked = true;
-            return true;
-        }
-
-        public static bool UnlockInspector(bool isLocked)
-        {
-            if (!isLocked) return false;
-            ActiveEditorTracker.sharedTracker.isLocked = false;
-            return false;
-        }
-
-        public static GUIStyle GetHeaderStyle(FontStyle fontStyle, int fontSize)
-        {
-            var headerStyle = new GUIStyle(GUI.skin.label)
-            {
-                fontStyle = fontStyle, fontSize = fontSize,
-                normal =
-                {
-                    textColor = EditorStyles.boldLabel.normal.textColor
-                }
-            };
-            return headerStyle;
-        }
-
         public static bool TryGetRaycastHit(out Vector3 hitPoint)
         {
             var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
