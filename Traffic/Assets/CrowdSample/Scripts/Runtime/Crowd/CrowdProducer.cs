a3396f370b25384731d1fc9ed4b829330278fe22
eos.chen <eos.chen@nadisystem.com>
2024-01-24 14:30:20 +0800

    新增 CrowdProducer
    + 用來分離 CrowdFactoryController 職責用的

---
+++ Traffic/Assets/CrowdSample/Scripts/Runtime/Crowd/CrowdProducer.cs
@@ -0,0 +1,53 @@
+﻿using CrowdSample.Scripts.Utils;
+using UnityEngine;
+
+namespace CrowdSample.Scripts.Runtime.Crowd
+{
+    public class CrowdProducer : MonoBehaviour, IUpdatable
+    {
+        #region Field Declarations
+
+        //
+
+        #endregion
+
+        #region Properties
+
+        //
+
+        #endregion
+
+        #region Unity Methods
+
+        //
+
+        #endregion
+
+        #region Public Methods
+
+        public void UpdateGizmo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        //
+
+        #endregion
+
+        #region Unity Event Methods
+
+        //
+
+        #endregion
+
+        #region Debug and Visualization Methods
+
+        //
+
+        #endregion
+    }
+}
