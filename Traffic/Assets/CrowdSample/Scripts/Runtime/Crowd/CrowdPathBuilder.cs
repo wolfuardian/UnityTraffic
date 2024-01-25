f726224f929f0376690fd6588845ca8f8db33797
eos.chen <eos.chen@nadisystem.com>
2024-01-25 14:02:49 +0800

    重構 將原本 PathGizmos 的職責整合進 CrowdPathBuilder 當中
    + 進行 region 劃分
    + 實現 IUpdateReceiver 方法

--- Traffic/Assets/CrowdSample/Scripts/Runtime/Crowd/CrowdPathBuilder.cs
+++ Traffic/Assets/CrowdSample/Scripts/Runtime/Crowd/CrowdPathBuilder.cs
@@ -1,21 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
+using CrowdSample.Scripts.Utils;
 
 namespace CrowdSample.Scripts.Runtime.Crowd
 {
     [RequireComponent(typeof(CrowdPath))]
-    [RequireComponent(typeof(CrowdPathGizmos))]
     [ExecuteInEditMode]
-    public class CrowdPathBuilder : MonoBehaviour
+    public class CrowdPathBuilder : MonoBehaviour, IUpdateReceiver
     {
-        #region Properties
+        #region Field Declarations
 
         [SerializeField] private CrowdPath crowdPath;
+        [SerializeField] private Vector2         arrowScale = new Vector2(2f, 2f);
 
         #endregion
 
         #region Properties
 
         public CrowdPath CrowdPath => crowdPath ??= GetComponent<CrowdPath>();
+        public Vector2   ArrowScale => arrowScale;
 
         #endregion
 
@@ -29,11 +31,54 @@ namespace CrowdSample.Scripts.Runtime.Crowd
         }
 
 
+        #region Unity Methods
+
 #if UNITY_EDITOR
         private void Awake()
         {
             if (crowdPath == null) crowdPath = GetComponent<CrowdPath>();
         }
+
 #endif
+
+        #endregion
+
+
+        #region Implementation Methods
+
+        public void UpdateImmediately()
+        {
+            FetchWaypoints();
+        }
+
+        #endregion
+
+        #region Debug and Visualization Methods
+
+        private void DrawAgentSpawnPoints()
+        {
+            foreach (var spawnData in crowdPath.AgentSpawnData)
+            {
+                GizmosUtils.ThicknessArrow(spawnData.position, spawnData.direction, Color.yellow, arrowScale);
+            }
+        }
+
+        private void DrawPathWaypoints()
+        {
+            var waypointCount        = waypoints.Count;
+            var actualWaypointsCount = crowdPath.IsClosedPath ? waypointCount : waypointCount - 1;
+
+            for (var i = 0; i < actualWaypointsCount; i++)
+            {
+                var nextIndex = (i + 1) % waypointCount;
+                if (waypoints[i] == null || waypoints[nextIndex] == null) continue;
+
+                Gizmos.DrawLine(waypoints[i].position, waypoints[nextIndex].position);
+                GizmosUtils.Arrow(waypoints[i].position, waypoints[nextIndex].position - waypoints[i].position,
+                    Color.cyan);
+            }
+        }
+
+        #endregion
     }
 }
