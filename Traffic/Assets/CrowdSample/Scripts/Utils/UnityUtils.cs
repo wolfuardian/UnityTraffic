49390a2491271d6e848a530b4ee487b1a9d7c899
eos.chen <eos.chen@nadisystem.com>
2024-01-29 12:17:25 +0800

    重構 通用程式碼
    + 重構計畫 A1

--- Traffic/Assets/CrowdSample/Scripts/Utils/UnityUtils.cs
+++ Traffic/Assets/CrowdSample/Scripts/Utils/UnityUtils.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
-using CrowdSample.Scripts.Runtime.Crowd;
-using UnityEditor;
-using UnityEditorInternal;
+﻿using System;
+using System.Linq;
 using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
 
 namespace CrowdSample.Scripts.Utils
 {
@@ -85,6 +87,59 @@ namespace CrowdSample.Scripts.Utils
 
                 Object.DestroyImmediate(remove);
             }
+        }
+
+        public static void UpdateAllReceiverImmediately()
+        {
+            foreach (var updatable in Object.FindObjectsOfType<MonoBehaviour>().OfType<IUpdateReceiver>())
+            {
+                updatable.UpdateImmediately();
+            }
+        }
+
+        public static void SetInspectorLock(bool lockState)
+        {
+            try
+            {
+                // Get the Inspector window type
+                var inspectorWindowType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+                if (inspectorWindowType == null)
+                {
+                    throw new InvalidOperationException("Could not find InspectorWindow type.");
+                }
+
+                // Get the current Inspector window instance
+                var inspectorWindow = EditorWindow.GetWindow(inspectorWindowType);
+                if (inspectorWindow == null)
+                {
+                    throw new InvalidOperationException("Could not find an open Inspector window.");
+                }
+
+                // Use reflection to set the lock state
+                var isLockedProperty =
+                    inspectorWindowType.GetProperty("isLocked", BindingFlags.Public | BindingFlags.Instance);
+                if (isLockedProperty == null)
+                {
+                    throw new InvalidOperationException("Could not find 'isLocked' property in InspectorWindow type.");
+                }
+
+                isLockedProperty.SetValue(inspectorWindow, lockState, null);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error setting Inspector lock state: " + ex.Message);
+            }
+        }
+
+        public static GUIStyle CreateHeaderStyle(FontStyle fontStyle, int fontSize)
+        {
+            var headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontStyle = fontStyle,
+                fontSize  = fontSize,
+                normal    = { textColor = EditorStyles.boldLabel.normal.textColor }
+            };
+            return headerStyle;
         }
     }
 }
