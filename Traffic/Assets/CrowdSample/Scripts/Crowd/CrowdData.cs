c51acb362384a187999430187366f8c427a159c9
eos.chen <eos.chen@nadisystem.com>
2024-01-30 14:34:02 +0800

    重構 優化並簡化資料結構

---
+++ Traffic/Assets/CrowdSample/Scripts/Crowd/CrowdData.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace CrowdSample.Scripts.Crowd
+{
+    [System.Serializable]
+    public struct SpawnPoint
+    {
+        [SerializeField] private Vector3 m_position;
+        [SerializeField] private Vector3 m_direction;
+        [SerializeField] private float   m_pathInterp;
+        [SerializeField] private float   m_pathLocation;
+
+        public SpawnPoint(Vector3 position, Vector3 direction, float pathInterp, float pathLocation)
+        {
+            m_position     = position;
+            m_direction    = direction;
+            m_pathInterp   = pathInterp;
+            m_pathLocation = pathLocation;
+        }
+
+        public Vector3 position
+        {
+            get => m_position;
+            set => m_position = value;
+        }
+
+        public Vector3 direction
+        {
+            get => m_direction;
+            set => m_direction = value;
+        }
+
+        public float pathInterp
+        {
+            get => m_pathInterp;
+            set => m_pathInterp = value;
+        }
+
+        public float pathLocation
+        {
+            get => m_pathLocation;
+            set => m_pathLocation = value;
+        }
+
+        public int targetPointNum => Mathf.Clamp(Mathf.FloorToInt(m_pathLocation) + 1, 0, int.MaxValue);
+    }
+}
