c22fb450d153c0ef7dcb6d77eb0e58e6c802bbe0
eos.chen <eos.chen@nadisystem.com>
2024-01-31 18:54:08 +0800

    新增 車牌模組 (wip)

---
+++ Traffic/Assets/CrowdSample/Scripts/Crowd/CrowdGateValidator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CrowdSample.Scripts.Crowd
+{
+    public class CrowdGateValidator : MonoBehaviour
+    {
+        #region Field Declarations
+
+        public enum PermissionID
+        {
+            Authorized,
+            Denied
+        }
+
+        private LicensePlateRegistry   licensePlateRegistry;
+        public  UnityEvent<CrowdAgent> onAgentEnterAccess;
+        public  UnityEvent<CrowdAgent> onAgentEnterDenied;
+        public  UnityEvent<CrowdAgent> onAgentExitDetect;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            licensePlateRegistry = new LicensePlateRegistry("Assets/AGVSample/Data/PlateNumber.csv");
+
+            // plateRegistry = new LicensePlateRegistry("Assets/AGVSample/Data/PlateNumber.csv");
+            // Debug.Log(plateRegistry.GetRandomPlateData().plateNumber);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var agentEntity = other.GetComponent<CrowdAgent>();
+
+            if (agentEntity == null) return;
+
+            var uid = agentEntity.userIdentity;
+
+            var plateData            = licensePlateRegistry.CheckPlateAuthorization(uid);
+
+            if (!Enum.TryParse(agentEntity.userIdentity, out PermissionID permissionId)) return;
+
+            switch (permissionId)
+            {
+                case PermissionID.Authorized:
+                    onAgentEnterAccess?.Invoke(agentEntity);
+                    break;
+                case PermissionID.Denied:
+                    onAgentEnterDenied?.Invoke(agentEntity);
+                    break;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            // var agentEntity = other.GetComponent<AgentEntity>();
+            // onAgentExitDetect?.Invoke(agentEntity);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Internal Methods
+
+        #endregion
+
+        #region Protected Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Unity Event Methods
+
+        #endregion
+
+        #region Implementation Methods
+
+        #endregion
+
+        #region Debug and Visualization Methods
+
+        #endregion
+
+        #region GUI Methods
+
+        #endregion
+    }
+}
---
+++ Traffic/Assets/CrowdSample/Scripts/Crowd/CrowdSpawnerLicensePlate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace CrowdSample.Scripts.Crowd
+{
+    public class CrowdSpawnerLicensePlate : MonoBehaviour
+    {
+        #region Field Declarations
+
+        [SerializeField] private CrowdSpawner         m_crowdSpawner;
+        [SerializeField] private string               m_licensePlateCsvPath;
+        private                  LicensePlateRegistry licensePlateRegistry;
+
+        #endregion
+
+        #region Properties
+
+        public CrowdSpawner crowdSpawner
+        {
+            get => m_crowdSpawner;
+            set => m_crowdSpawner = value;
+        }
+
+        public string licensePlateCsvPath
+        {
+            get => m_licensePlateCsvPath;
+            set => m_licensePlateCsvPath = value;
+        }
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            if (licensePlateRegistry == null) return;
+            licensePlateRegistry = new LicensePlateRegistry(licensePlateCsvPath);
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        #endregion
+    }
+}
